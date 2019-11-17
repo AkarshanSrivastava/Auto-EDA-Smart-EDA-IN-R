b3bb0edb647a10edb0f39e2ea75adbb56e2298f6
AkarshanSrivastava <48011869+AkarshanSrivastava@users.noreply.github.com>
2019-11-17 16:38:23 +0530

    Update autoeda rtutor package.R

--- autoeda rtutor package.R
+++ autoeda rtutor package.R
@@ -4,13 +4,15 @@ library(rJava)
 library(RtutoR)
 
 
-# The function takes 3 mandatory arguments
-# df - A dataframe object to be analyzed 
-# target_var - The name of the target (or dependent) variable
-# output_file_name = Output file name and destination path (output file is a PowerPoint deck - if only file name is provided, the file is saved in the current working directory)
-df = read.csv("C://Users//Sanchi//Desktop//Assignment//Complete//R//logistic regression//bank-full.csv",sep = ';') # Load the Titanic dataset
+
+df = read.csv("C://Users//Desktop//Assignment//Complete//R//logistic regression//bank-full.csv",sep = ';') # Load the dataset
+#Feature selection
 df <- df[,-c(9,11,10,13,14,15,16)]
 
+#df=Data Frame
+# target_var - The name of the target (or dependent) variable
+# output_file_name = Output file name and destination path (output file is a PowerPoint deck - if only file name is provided, the file is saved in the current working directory)
 res = generate_exploratory_analysis_ppt(df,target_var = "y",
-                                        output_file_name = "C://Users//Sanchi//Desktop//Assignment//Complete//R//logistic regression//final.pptx")
+                                        output_file_name = "C://Users//Desktop//Assignment//Complete//R//logistic regression//final.pptx")
+#For R shiny (web)
 gen_exploratory_report_app(df)
